664765e27e7caf4a8275757d47cb8a35a7559afd
Marco <marcovelardejr@gmail.com>
2022-04-23 17:10:31 -0500

    commit tree 989e717983090c4a846ba0eb4212c6cd399455ca

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    let messages = []; 
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+
+}
+
+function countDown (positiveInteger) {
+    while(positiveInteger > -1) {
+        console.log(positiveInteger--);
+    }
+}
+
+
